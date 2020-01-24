34ba5acb0a5a3fa4942933e8935c1cb9a9e784fb
Andrey Mokretsov <am@kubia.com>
2020-01-24 18:05:23 +0500

    Fix

--- src/Kubia/Camunda/CamundaBaseConnector.php
+++ src/Kubia/Camunda/CamundaBaseConnector.php
@@ -223,7 +223,7 @@ abstract class CamundaBaseConnector
             Logger::elastic('bpm',
                 'in progress',
                 'error',
-                $this->message['data'] ?? (object)[],
+                (object)($this->message['data'] ?? []),
                 (object)[],
                 ['type' => 'system', 'message' => $message],
                 $this->channelLog,
